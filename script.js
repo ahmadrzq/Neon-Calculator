41fb2b3d67a2e8e9903f5ffb27ad0f05fd38e41d
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-03-25 08:06:32 +0700

    Set content of layer to 0 when window loads

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 // Selecting all elements that have class "button" and the element with id "layerNumber"
 const button = document.querySelectorAll(".button");
 const layer = document.querySelector("#layerNumber");
+
+// When the window loads, set the content of the element with id "layerNumber" to "0"
+window.addEventListener("load", () => {
+  layer.innerHTML = "0";
+});
+
