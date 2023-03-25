1ee5899ace8779abce47a629d504bb8b701331e4
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-03-25 08:05:22 +0700

    Select element button and layer

---
+++ script.js
@@ -0,0 +1,3 @@
+// Selecting all elements that have class "button" and the element with id "layerNumber"
+const button = document.querySelectorAll(".button");
+const layer = document.querySelector("#layerNumber");
