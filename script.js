4cc7c13e53a3b4390dadfee2c79aab40814f994b
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-03-25 08:08:18 +0700

    Add event click to the button element

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ window.addEventListener("load", () => {
   layer.innerHTML = "0";
 });
 
+// For each button element, add a click event listener
+for (let i = 0; i < button.length; i++) {
+  button[i].addEventListener("click", () => {
+    // Log the button that was clicked to the console
+    console.log(`Klik button : ${button[i].innerHTML}`);
+    // Call the deleteZero function to remove leading zeros
+    deleteZero();
+    // Add the button's content to the end of the layer element's content
+    layer.innerHTML += button[i].innerHTML;
+  });
+}
+
